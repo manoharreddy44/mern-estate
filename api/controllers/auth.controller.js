081c2a084a30d3592f769f59cd55b3a97ad38016
Manohar <manoharreddy.koppala@gmail.com>
2024-08-04 21:06:24 +0530

    complete sign up page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,8 +3,14 @@ import bcrypt from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 
 
+
+
+
 export const signup = async(req,res,next) =>{
+
     const {username,email, password} = req.body;
+
+    
 
     const hashedPassword = bcrypt.hashSync(password,10);
 
@@ -14,13 +20,15 @@ export const signup = async(req,res,next) =>{
         password : hashedPassword
     });
 
+
     try{
+
         await newUser.save();
         res.status(201).json("User created successfully");
     }
     catch(error){
+        console.log(error);
         next(errorHandler(550,'declaring error by function'));
     }
-
     
 }
